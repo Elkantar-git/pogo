1374716c656cc7a820ebc030a3d6142e0ae9d700
Elkantar <3000azer@gmail.com>
2022-02-28 13:30:53 +0100

    Noe's Phone

--- yolov5_prod.py
+++ yolov5_prod.py
@@ -25,7 +25,7 @@ def yoloSearchPokemon(model):
     while True:
         try:
                 # Image
-            img = ImageGrab.grab(bbox =(1024, 664, 1856, 1496))  # take a screenshot bbox=(xmin, ymin, xmax, ymax)
+            img = ImageGrab.grab(bbox =(1024, 624, 1856, 1456))  # take a screenshot bbox=(xmin, ymin, xmax, ymax)
             imgGREY = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
             x, y = results(imgGREY, model)
             break
@@ -56,7 +56,7 @@ def results(image, model):
             centerY = y1 + ((y2 - y1)/2)
                 # Position on screen
             posX = (centerX + 1024)/2
-            posY = (centerY + 684)/2
+            posY = (centerY + 624)/2
             acc = con * 100
             # print(cs, con, x1, y1, xcenter, ycenter, xpos, ypos)
             print(f'Locate position in [{posX}, {posY}] with an accuracy of : {acc} %')
@@ -79,17 +79,17 @@ def catchPokemonOrQuit(x, y):
 
     # Verify the page
     while poke or stop or arena or rocket or rocket_battle or onMap == None:
-        poke = pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1750, 1500, 150, 150))
+        poke = pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1680, 1460, 140, 140)) # , region=(1750, 1500, 150, 150)
         # Catch pokemon
         if poke != None:
             print('Catching Pokemon...')
-            while pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1750, 1500, 150, 150)) != None:
+            while pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1680, 1460, 140, 140)) != None:
                 pyautogui.moveTo(725, 825)
                 pyautogui.dragTo(725, 600, 0.2, button='left')
                 time.sleep(1)
                 while catch or onMap == None:
-                    catch = pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1750, 1500, 150, 150))
-                    onMap = pyautogui.locateCenterOnScreen('pogo/locate_img/menu.jpg', grayscale=True, confidence=.8, region=(1380, 1600, 130, 130))
+                    catch = pyautogui.locateCenterOnScreen('pogo/locate_img/ball.jpg', grayscale=True, confidence=.8, region=(1680, 1460, 140, 140))
+                    onMap = pyautogui.locateCenterOnScreen('pogo/locate_img/menu.jpg', grayscale=True, confidence=.8) # , region=(1380, 1600, 130, 130)
                     if catch != None:
                         compteur += 1
                         print('Pokemon: NOPE... try again !')
@@ -102,16 +102,16 @@ def catchPokemonOrQuit(x, y):
                         break
             break
 
-        onMap = pyautogui.locateCenterOnScreen('pogo/locate_img/menu.jpg', grayscale=False, confidence=.8, region=(1380, 1600, 130, 130))
+        onMap = pyautogui.locateCenterOnScreen('pogo/locate_img/menu.jpg', grayscale=False, confidence=.8)
         if onMap != None:
             print('ERROR_YOLO: This is the Map')
             break
 
-        stop = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_menu.jpg', grayscale=False, confidence=.8, region=(1400, 1600, 100, 100))
+        stop = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_menu.jpg', grayscale=False, confidence=.8) # , region=(1400, 1600, 100, 100)
             # Quit pokestop
         if stop != None:
             print('PokeStop Detected')
-            stopxy = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_menu.jpg', grayscale=False, confidence=.8, region=(1400, 1600, 100, 100))
+            stopxy = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_menu.jpg', grayscale=False, confidence=.8)
             pyautogui.click(stopxy[0]/2, stopxy[1]/2)
             print('Quit PokeStop')
             time.sleep(1)
@@ -161,11 +161,11 @@ def catchPokemonOrQuit(x, y):
                 
             break
 
-        arena = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_arena.jpg', grayscale=False, confidence=.8, region=(1380, 1620, 150, 150))
+        arena = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_arena.jpg', grayscale=False, confidence=.8) # , region=(1380, 1620, 150, 150)
         # Quit arena
         if arena != None:
             print('Arena Detected')
-            arenaxy = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_arena.jpg', grayscale=False, confidence=.8, region=(1380, 1620, 150, 150))
+            arenaxy = pyautogui.locateCenterOnScreen('pogo/locate_img/cross_arena.jpg', grayscale=False, confidence=.8)
             pyautogui.click(arenaxy[0]/2, arenaxy[1]/2)
             print('Quit Arena')
             break
@@ -264,7 +264,6 @@ while True:
 
     catchPokemonOrQuit(x, y)
 
-    time.sleep(2)
     
 
 
